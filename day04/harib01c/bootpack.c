ca8c97506ebcf87328f8342e470ccaf5f54fa5d8
pollenJP <polleninjp@gmail.com>
2019-02-11 11:51:55 +0900

    Update day04/harib01c/

--- day04/harib01c/bootpack.c
+++ day04/harib01c/bootpack.c
@@ -1,22 +1,23 @@
-// bootpack.c
-
-extern void io_hlt(void);
-// extern void write_mem8(int addr, int data);
+extern void _io_hlt(void);
+extern void _write_mem8(int addr, int data);
 
 void HariMain(void)
+// > 「30日でできる!OS自作入門」 p66
+// > おっと大事なことを忘れていました。関数名HariMainですが、これは非常に意味ある名前で、この名前の関数から
+// > プログラムが始まるということになっています。だからこれを違う名前に変えることはできません。
 {
-  char *p;  // BYTE [...]用番地
-
-  for (int i = 0xa0000; i <= 0xaffff; i++){
-    p = (char *) i;
-    *p = i & 0x0f;
-    // write_mem8(i, i & 0x0f);
-    // または
-    // *((char *) i) = i & 0x0f;
-    // と同じ処理
+  int i;    // VRAM用番地指定変数, 32bit(4 Byte)番地
+  char *p;  // *pは 1 Byte値, pは32bit(4 Byte)番地
+  for (i = 0xa0000; i <= 0xaffff; i++){
+    // > VRAMは0xa0000～0xaffffの64KBです。厳密に言うと、320x200=64000なので、62.5KBですが.
+    // > [INT(0x10); ビデオ関係](http://oswiki.osask.jp/?%28AT%29BIOS#n5884802)
+    // 　なおこのグラフィックバッファの開始番地 0xaffff は `asmhead.asm` ないで メモリアドレスを指す変数 VRAM(=0x0ff8)
+    // に保存されている. (`DWORD [VRAM]`)
+    p = (char *) i;  // 番地の代入
+    *p = i & 0x0f;   // 0-15 の値を期待するものである.
   }
 
   for(;;){
-    io_hlt();
+    _io_hlt();
   }
 }
