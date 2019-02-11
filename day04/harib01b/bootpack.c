b87690ba81afe03c667f0c25c608d260a8a53819
pollenJP <polleninjp@gmail.com>
2019-02-11 11:32:13 +0900

    Update day04/harib01b/

--- day04/harib01b/bootpack.c
+++ day04/harib01b/bootpack.c
@@ -1,15 +1,21 @@
-// bootpack.c
-
-extern void io_hlt(void);
-extern void write_mem8(int addr, int data);
+extern void _io_hlt(void);
+extern void _write_mem8(int addr, int data);
 
 void HariMain(void)
+// > 「30日でできる!OS自作入門」 p66
+// > おっと大事なことを忘れていました。関数名HariMainですが、これは非常に意味ある名前で、この名前の関数から
+// > プログラムが始まるということになっています。だからこれを違う名前に変えることはできません。
 {
-  for (int i = 0xa0000; i <= 0xaffff; i++){
-    write_mem8(i, i & 0x0f);    // 16画素(下位4bit)ごとに色変更
+  int i;
+  for (i = 0xa0000; i <= 0xaffff; i++){
+    // > VRAMは0xa0000～0xaffffの64KBです。厳密に言うと、320x200=64000なので、62.5KBですが.
+    // > [INT(0x10); ビデオ関係](http://oswiki.osask.jp/?%28AT%29BIOS#n5884802)
+    // 　なおこのグラフィックバッファの開始番地 0xaffff は `asmhead.asm` ないで メモリアドレスを指す変数 VRAM(=0x0ff8)
+    // に保存されている. (`DWORD [VRAM]`)
+    _write_mem8(/* int addr= */ i, /* int data = */ i & 0x0f);  // 第2引数dataは 0-15 の値を期待するものである.
   }
 
   for(;;){
-    io_hlt();
+    _io_hlt();
   }
 }
